6307283553431d1d4250dc0796b43e78426d167e
Gaëtan <gaetan.serre@xn--universit-paris-sacaly-j8b.fr>
2020-12-29 18:01:50 +0100

    Optimized AI

--- src/classes/board/board.h
+++ src/classes/board/board.h
@@ -23,7 +23,7 @@ class Board {
 
         Piece* squares[64];
 
-        void init(string fen = "1k2r3/8/8/8/8/8/5PPP/R3q2K w - - 0 1");
+        void init(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         void printPieces();
         void printLegalMoves();
 
